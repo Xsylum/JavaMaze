c6f5a40c038c8ab4662ef611d0f9e73754924100
Xander Fleury <fleuryxander@gmail.com>
2023-04-10 21:36:41 -0400

    Implement DoublyLinkedList methods

--- DoublyLinkedList.java
+++ DoublyLinkedList.java
@@ -1,6 +1,11 @@
+/**
+ *			TO WRITE
+ */
 public class DoublyLinkedList<E> implements List<E> {
 
-	
+	/**
+	 *			TO WRITE
+	 */
 	private static class Node<T> {
 		
 		T value;
@@ -18,13 +23,20 @@ public class DoublyLinkedList<E> implements List<E> {
 	private Node<E> head;
 	private int size;
 	
+	/**
+	 *			TO WRITE
+	 */
 	public DoublyLinkedList() {
 		head = new Node<E>(null, null, null); // Dummy node
 		head.next = head;
 		head.previous = head;
-		size = -1; // Sentinel value;
+		size = 0; // Sentinel value;
 	}
 	
+	
+	/**
+	 *			TO WRITE
+	 */
 	public void addFirst(E elem) {
 		
 		if (elem == null) {
@@ -41,42 +53,221 @@ public class DoublyLinkedList<E> implements List<E> {
 			head.next.next.previous = newNode;
 		}
 		
+		size++;
+		
 	}
 	
+	
+	/**
+	 *			TO WRITE
+	 */
 	public void add(E elem) {
-		throw new UnsupportedOperationException("method not implemented!");
+		
+		if (elem == null) {
+			throw new NullPointerException("No null values in list!");
+		}
+		
+		if (head.next.value == null) { // Only dummy node in the list!
+			Node<E> firstNode = new Node<E>(elem, head, head);
+			head.next = head.previous = firstNode;
+		} else {
+			head.previous = new Node<E>(elem, head.previous, head);
+			head.previous.previous.next = head.previous;
+		}
+		
+		size++;
 	}
 	
+	
+	/**
+	 *			TO WRITE
+	 */
 	public E removeLast() {
-		throw new UnsupportedOperationException("method not implemented!");
+		
+		if (isEmpty()) {
+			throw new IllegalStateException("Cannot remove from empty list!");
+		}
+		
+		E output;
+		output = head.previous.value;
+		head.previous = head.previous.previous;
+		head.previous.next = head;
+		
+		size--;
+		return output;
+		
+		
 	}
 	
+	
+	/**
+	 *			TO WRITE
+	 */
 	public E removeFirst() {
-		throw new UnsupportedOperationException("method not implemented!");
+		
+		if (isEmpty()) {
+			throw new IllegalStateException("Cannot remove from empty list!");
+		}
+		
+		E output;
+		output = head.next.value;
+		head.next = head.next.next;
+		head.next.previous = head;
+		
+		size--;
+		return output;
 	}
 	
+	/**
+	 * I wonder if this can be sped up via an iteration system or even
+	 * changing whether we look forwards or backward depending on if 
+	 * the index is in the front or backhalf of the list
+	 */
 	public E remove(int index) {
-		throw new UnsupportedOperationException("method not implemented!");
+		
+		if (isEmpty()) {
+			throw new IllegalStateException("Cannot remove from empty list!");
+		}
+		
+		if (index < 0 || index >= size) {
+			throw new IllegalArgumentException("Index out of bounds!");
+		}
+		
+		int i = 0;
+		Node<E> cursor = head;
+		
+		while (i != index) {
+			cursor = cursor.next;
+			i++;
+		}
+		
+		E output = cursor.next.value;
+		cursor.next = cursor.next.next;
+		cursor.next.previous = cursor;
+		
+		size--;
+		return output;
+		
+		
 	}
 	
+	
+	/**
+	 *			TO WRITE
+	 */
 	public void remove(E value) {
-		throw new UnsupportedOperationException("method not implemented!");
+		
+		if (isEmpty()) {
+			throw new IllegalStateException("Cannot remove from empty list!");
+		}
+		
+		Node<E> cursor = head;
+		
+		while (cursor.next.value != null && !(cursor.next.value.equals(value))) {
+			cursor = cursor.next;
+		}
+		
+		if (cursor.next.value == null) {
+			throw new IllegalArgumentException("No element with value: " + value + " in list!");
+		}
+		
+		cursor.next = cursor.next.next;
+		cursor.next.previous = cursor;
+		
+		size--;
+		
 	}
 	
+	
+	/**
+	 *			TO WRITE
+	 */
 	public E get(int index) {
-		throw new UnsupportedOperationException("method not implemented!");
+		
+		if (isEmpty()) {
+			throw new IllegalStateException("Cannot get from empty list!");
+		}
+		
+		if (index < 0 || index >= size) {
+			throw new IllegalArgumentException("Index out of Bounds!");
+		}
+		
+		int i = 0;
+		Node<E> cursor = head;
+		
+		while (i != index) {
+			cursor = cursor.next;
+			i++;
+		}
+		
+		E output = cursor.next.value;
+		return output;
 	}
 	
+	
+	/**
+	 *			TO WRITE
+	 */
 	public int indexOf(E value) {
-		throw new UnsupportedOperationException("method not implemented!");
+		
+		if (isEmpty()) {
+			throw new IllegalStateException("Cannot get index of element for empty list!");
+		}
+		
+		int i = 0;
+		Node<E> cursor = head;
+		
+		while (cursor.next.value != null && !(cursor.next.value.equals(value))) {
+			cursor = cursor.next;
+			i++;
+		}
+		
+		if (cursor.next.value == null) {
+			throw new IllegalArgumentException("No element in list with value: " + value + " in list!");
+		}
+		
+		return i;
 	}
 	
+	
+	/**
+	 *			TO WRITE
+	 */
 	public int size() {
 		return size;
 	}
 	
+	
+	/**
+	 *			TO WRITE
+	 */
 	public boolean isEmpty() {
-		return (size == -1);
+		return (size == 0);
+	}
+	
+	
+	/**
+	 *			TO WRITE
+	 */
+	public String toString() {
+		
+		StringBuffer output = new StringBuffer("[");
+		Node<E> cursor = head;
+		
+		while (cursor.next.value != null) {
+			output.append(cursor.next.value);
+			
+			if (cursor.next.next.value != null) { // Adds comma when the while loop will run for cursor.next
+				output.append(", ");
+			}
+			
+			cursor = cursor.next;
+		}
+		
+		output.append("]");
+		
+		return output.toString();
+		
 	}
 	
 
